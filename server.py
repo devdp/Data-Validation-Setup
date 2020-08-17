37f39ec836cd51bb8973d3845993f8203da53eba
Ubuntu <ubuntu@ip-172-31-20-141.us-east-2.compute.internal>
2020-08-17 20:54:50 +0000

    search added with flaws

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 import data_storage
 import datetime
 
@@ -8,15 +8,15 @@ app = Flask(__name__)
 def login():
     global user_df
     user_df = data_storage.get_data_user()
-    return render_template('login.html')
+    return render_template('login.html',error=' ')
 
 @app.route('/index', methods = ['GET','POST'])
 def index():
     global username
     username = request.form['username']
     pasw = request.form['pass']
-    role = df.loc[(user_df['username']==username) & (user_df['passw']==pasw),'role'].iloc[0]
-    if len(df[(user_df['username']==username) & (user_df['passw'] == pasw)])>0:
+    role = user_df.loc[(user_df['username']==username) & (user_df['passw']==pasw),'role'].iloc[0]
+    if len(user_df[(user_df['username']==username) & (user_df['passw'] == pasw)])>0:
         return render_template('index.html',user = role)
     else:
         return render_template('login.html',error = 'Invalid Creds!')
@@ -35,9 +35,14 @@ def update():
 
 @app.route('/getdata', methods=['GET','POST'])
 def getdata():
-    data = data_storage.get_data()
-    data = data.to_json(orient='records')
-    return data
+    df = data_storage.get_data()
+    a = {}
+    for i in range(0,len(df)):
+        a1 = {"user":df['user'][i],"timestamp":df['timestamp'][i],"order_number":df['order_number'][i],"trans_id":df['trans_id'][i],"Amount":df['Amount'][i]}
+        s1 = {"s{}".format(i):a1}
+        a.update(s1)
+    data = {"data":a}
+    return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=7003)
